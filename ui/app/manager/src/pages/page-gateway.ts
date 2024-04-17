649f09e3e0c8b233f08c770f4e2ecfb80d29fb70
MartinaeyNL <business@martinaey.nl>
2024-04-17 15:30:12 +0200

    Added basic version of interconnect UI

--- ui/app/manager/src/pages/page-gateway.ts
+++ ui/app/manager/src/pages/page-gateway.ts
@@ -1,12 +1,12 @@
 import {css, html, PropertyValues, TemplateResult, unsafeCSS} from "lit";
 import {customElement, property, state} from "lit/decorators.js";
+import {until} from "lit/directives/until.js";
 import i18next from "i18next";
 import "@openremote/or-components/or-panel";
 import {ClientRole, ConnectionStatus, GatewayConnection, GatewayConnectionStatusEvent} from "@openremote/model";
-import manager, {DefaultColor1, DefaultColor3, OREvent} from "@openremote/core";
+import manager, {DefaultColor1, DefaultColor3} from "@openremote/core";
 import {InputType, OrInputChangedEvent} from "@openremote/or-mwc-components/or-mwc-input";
-import {Page, PageProvider} from "@openremote/or-app";
-import {AppStateKeyed} from "@openremote/or-app";
+import {AppStateKeyed, Page, PageProvider} from "@openremote/or-app";
 import {Store} from "@reduxjs/toolkit";
 
 export function pageGatewayProvider(store: Store<AppStateKeyed>): PageProvider<AppStateKeyed> {
@@ -53,8 +53,8 @@ export class PageGateway extends Page<AppStateKeyed>  {
                 margin: 20px auto 0;
                 font-size: 18px;
                 font-weight: bold;
-                max-width: 1000px;
-                min-width: 600px;
+                max-width: 1360px;
+                width: 100%;
                 color: var(--or-app-color3, ${unsafeCSS(DefaultColor3)});
             }
 
@@ -65,35 +65,56 @@ export class PageGateway extends Page<AppStateKeyed>  {
             
             or-panel {
                 position: relative;
-                max-width: 1000px;
-                min-width: 600px;
+                max-width: 1360px;
+                width: 100%;
                 margin: 20px auto;
             }
             
-            #status {
+            #gateway-status-header {
                 position: absolute;
                 top: 15px;
                 right: 25px;
                 font-weight: bold;  
             }
             
-            #fields {
+            #gateway-content {
+                display: flex;
+                flex-wrap: wrap;
+                padding: 10px;
+                gap: 40px;
+            }
+            
+            .gateway-column {
+                flex: 1;
+                flex-basis: 45%;
+                min-width: 350px;
                 display: flex;
                 flex-direction: column;
-                padding: 10px;
+                gap: 20px;
             }
             
-            #fields > or-mwc-input {
-                margin: 10px 0;
-                width: 100%;
+            .gateway-sharing-control {
+                display: flex;
+                flex-direction: column;
+                gap: 10px;
             }
             
-            #buttons {
-                text-align: right;
+            .gateway-sharing-control-child {
+                margin-left: 20px;
+                display: flex;
+                gap: 10px;
+                align-items: center;
             }
             
-            #buttons > or-mwc-input {
-                margin: 10px;                
+            #gateway-footer {
+                margin-top: 40px;
+                display: flex;
+                justify-content: space-between;
+            }
+            
+            #gateway-footer > div {
+                display: flex;
+                gap: 10px;
             }
 
             @media only screen and (max-width: 780px){
@@ -108,6 +129,12 @@ export class PageGateway extends Page<AppStateKeyed>  {
                 #title {
                     width: 100%;
                     min-width: auto;
+                }
+            }
+            
+            @media only screen and (max-width: 1200px) {
+                #gateway-content {
+                    gap: 20px;
                 }
             }
         `;
@@ -168,7 +195,7 @@ export class PageGateway extends Page<AppStateKeyed>  {
     }
 
     protected render(): TemplateResult | void {
-        const connection = this._connection;
+        const disabled = this._loading || this._readonly;
 
         return html`
             <div id="wrapper">
@@ -176,24 +203,105 @@ export class PageGateway extends Page<AppStateKeyed>  {
                     <or-icon icon="cloud"></or-icon>${i18next.t("gatewayConnection")}
                 </div>
                 <or-panel ?disabled="${this._loading}" .heading="${i18next.t("connectionDetails")}">
-                    <div id="status">
-                        ${this._connectionStatus}
-                    </div>
-                    <div id="fields">
-                        <or-mwc-input .label="${i18next.t("host")}" .type="${InputType.TEXT}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.host : ""}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("host", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("port")}" .type="${InputType.NUMBER}" ?disabled="${this._loading || this._readonly}" min="1" max="65536" step="1" .value="${connection ? connection.port : undefined}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("port", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("realm")}" .type="${InputType.TEXT}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.realm : ""}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("realm", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("clientId")}" .type="${InputType.TEXT}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.clientId : ""}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("clientId", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("clientSecret")}" .type="${InputType.TEXT}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.clientSecret : ""}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("clientSecret", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("secured")}" .type="${InputType.SWITCH}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.secured : false}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("secured", e.detail.value)}"></or-mwc-input>
-                        <or-mwc-input .label="${i18next.t("disabled")}" .type="${InputType.SWITCH}" ?disabled="${this._loading || this._readonly}" .value="${connection ? connection.disabled : false}" @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("disabled", e.detail.value)}"></or-mwc-input>
-                    </div>
-                    <div id="buttons">
-                        <or-mwc-input label="reset" ?disabled="${!this._dirty || this._loading || this._readonly}" .type="${InputType.BUTTON}" raised @click="${() => this._reset()}"></or-mwc-input>                
-                        <or-mwc-input label="delete" ?disabled="${this._loading || this._readonly}" .type="${InputType.BUTTON}" raised @click="${() => this._delete()}"></or-mwc-input>                
-                        <or-mwc-input label="save" ?disabled="${!this._dirty || this._loading || this._readonly}" .type="${InputType.BUTTON}" raised @click="${() => this._save()}"></or-mwc-input>                
-                    </div>
+                    
+                    ${until(this.getHeaderTemplate(this._connectionStatus, disabled))}
+
+                    ${until(this.getContentTemplate(this._connection, disabled))}
+                    
+                    ${until(this.getFooterTemplate(disabled, this._dirty))}
                 </or-panel>
+            </div>
+        `;
+    }
+
+    protected async getHeaderTemplate(connectionStatus: ConnectionStatus, _disabled = true): Promise<TemplateResult> {
+        return html`
+            <div id="gateway-status-header">
+                ${connectionStatus}
+                <or-mwc-input .type="${InputType.BUTTON}" label="JSON" outlined icon="pencil"></or-mwc-input>
+            </div>
+        `;
+    }
+
+    protected async getContentTemplate(connection: GatewayConnection, disabled = true): Promise<TemplateResult> {
+        return html`
+            <div id="gateway-content">
+                ${until(this.getSettingsColumns(connection, disabled), html`<or-loading></or-loading>`)}
+                ${until(this.getSharingColumns(connection, disabled), html`<or-loading></or-loading>`)}
+            </div>
+        `;
+    }
+
+    protected async getSettingsColumns(connection: GatewayConnection, disabled = true): Promise<TemplateResult> {
+        return html`
+            <div id="gateway-column-1" class="gateway-column">
+                <h5 style="margin-bottom: 0">
+                    <or-translate value="settings"></or-translate>
+                </h5>
+                <or-mwc-input .label="${i18next.t("host")}" .type="${InputType.TEXT}" ?disabled="${disabled}" .value="${connection?.host}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("host", e.detail.value)}"
+                ></or-mwc-input>
+                <or-mwc-input .label="${i18next.t("port")}" .type="${InputType.NUMBER}" ?disabled="${disabled}" min="1" max="65536" step="1" .value="${connection?.port}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("port", e.detail.value)}"
+                ></or-mwc-input>
+                <or-mwc-input .label="${i18next.t("realm")}" .type="${InputType.TEXT}" ?disabled="${disabled}" .value="${connection?.realm}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("realm", e.detail.value)}"
+                ></or-mwc-input>
+                <or-mwc-input .label="${i18next.t("disabled")}" .type="${InputType.CHECKBOX}" ?disabled="${disabled}" .value="${connection?.disabled || false}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("disabled", e.detail.value)}"
+                ></or-mwc-input>
+            </div>
+
+            <div id="gateway-column-2" class="gateway-column">
+                <h5 style="margin-bottom: 0">
+                    <or-translate value=""></or-translate>
+                </h5>
+                <or-mwc-input .label="${i18next.t("clientId")}" .type="${InputType.TEXT}" ?disabled="${disabled}" .value="${connection?.clientId}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("clientId", e.detail.value)}"
+                ></or-mwc-input>
+                <or-mwc-input .label="${i18next.t("clientSecret")}" .type="${InputType.TEXT}" ?disabled="${disabled}" .value="${connection?.clientSecret}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("clientSecret", e.detail.value)}"
+                ></or-mwc-input>
+                <or-mwc-input .label="${i18next.t("secured")}" .type="${InputType.CHECKBOX}" style="height: 56px;" ?disabled="${disabled}" .value="${connection?.secured || false}"
+                              @or-mwc-input-changed="${(e: OrInputChangedEvent) => this._setConnectionProperty("secured", e.detail.value)}"
+                ></or-mwc-input>
+            </div>
+        `;
+    }
+
+    protected async getSharingColumns(_connection: GatewayConnection, disabled = true): Promise<TemplateResult> {
+        return html`
+            <div id="gateway-column-3" class="gateway-column">
+                <h5 style="margin-bottom: 0">
+                    <or-translate value="details"></or-translate>
+                </h5>
+                <div class="gateway-sharing-control">
+                    <or-mwc-input .label="${i18next.t("gateway.limit_sharing_attribute")}" .type="${InputType.CHECKBOX}" ?disabled="${disabled}"></or-mwc-input>
+                    <div class="gateway-sharing-control-child">
+                        <or-mwc-input label="X asset types selected" .type="${InputType.BUTTON}" raised ?disabled="${disabled}"></or-mwc-input>
+                    </div>
+                </div>
+                <div class="gateway-sharing-control">
+                    <or-mwc-input .label="${i18next.t("gateway.limit_sharing_rate")}" .type="${InputType.CHECKBOX}" ?disabled="${disabled}"></or-mwc-input>
+                    <div class="gateway-sharing-control-child">
+                        <or-mwc-input .type="${InputType.NUMBER}" compact outlined style="width: 84px;"></or-mwc-input>
+                        <span>minutes interval .............</span>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+
+    protected async getFooterTemplate(disabled = true, changed = false): Promise<TemplateResult> {
+        return html`
+            <div id="gateway-footer">
+                <div id="gateway-footer-start">
+                    <or-mwc-input label="delete" ?disabled="${disabled}" .type="${InputType.BUTTON}" outlined @click="${() => this._delete()}"></or-mwc-input>
+                    <or-mwc-input label="reset" ?disabled="${!changed || disabled}" .type="${InputType.BUTTON}" outlined @click="${() => this._reset()}"></or-mwc-input>
+                </div>
+                <div id="gateway-footer-end">
+                    <or-mwc-input label="save" ?disabled="${!changed || disabled}" .type="${InputType.BUTTON}" raised @click="${() => this._save()}"></or-mwc-input>
+                </div>
             </div>
         `;
     }
